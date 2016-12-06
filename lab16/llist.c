60d7e486cfcedda832a09fef0f473b6ff0220abf
tjeffries <scipio.jet@gmail.com>
2016-12-06 16:07:47 -0500

    began lab 16

---
+++ lab16/llist.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+	printf("this\n");
+
+	/* srtcmp: return < 0 if s < t, 0 if s==t, > 0 if s > t */
+	int mystrcmp(char *s, char *t){
+		
+	}
+}
