a8f1afee9caba20638294e32cbc625966c9df101
tjeffries <scipio.jet@gmail.com>
2017-03-23 01:13:16 -0400

    misc directory and fizzbuzz implementation

---
+++ 173Project2/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: Feb 27, 2017
+ *      Author: thomas
+ */
+
+
+#include <stdio.h>
+
+int main(int argc, char **argv, char **envp){
+	printf("hello world\n");
+	printf("this is a message\n");
+
+	return 0;
+}
